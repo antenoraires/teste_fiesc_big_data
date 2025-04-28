110aaf87ce4afe31c548d9d9b0dc95977828de77
antenoraires <antenoraires37@gmail.com>
2025-04-27 23:18:15 -0400

    init

---
+++ try_module.py
@@ -0,0 +1,42 @@
+from minio import Minio
+from fiesc_big_data import Pipeline
+from fiesc_big_data.control import seccao_pyspark
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+# Carrega as variáveis do arquivo .env
+load_dotenv()
+
+# Acessa as variáveis
+
+access_key = os.getenv('ACESSE_KEY_MINIO')
+secret_key = os.getenv('ACESSE_SECRET_MINIO')
+url_minio = os.getenv('ACESSE_URL_MINIO')
+
+# Configurações do MinIO
+minio_client = Minio(
+    url_minio,  # Endereço do servidor MinIO atualizado
+    access_key=access_key,  # Substitua pelo seu access key
+    secret_key=secret_key,  # Substitua pelo seu secret key
+    secure=False  # True se estiver usando HTTPS
+)
+
+engine = create_engine("postgresql://meuuser:minhasenha@localhost:5432/meubanco")
+
+spark = seccao_pyspark("Fiesc", minio_client)
+
+# Intanciar a classe Pipeline
+pipeline = Pipeline(minio_client, engine, spark)
+# Ingestão de dados
+pipeline.ingest(pipeline.minio_client, pipeline.config)
+# Transformação de dados
+pipeline.transform(pipeline.minio_client, pipeline.config)
+# Aplicação de regras
+pipeline.rules(pipeline.minio_client, pipeline.config, pipeline.spark)
+# carregando no DW
+pipeline.load(pipeline.engine, pipeline.spark)
+
+# Encerrando sessão do Spark
+print("Encerrando sessão do Spark...")
+spark.stop()
